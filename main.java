30651a53900c449ae9a6efd0deade336465a232a
Arul Pragasam <arulpragasam2003@gmail.com>
2026-04-08 12:35:35 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,20 @@
+import com.sun.net.httpserver.*;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+public class main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(3000), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello from Java Web Server!";
+            exchange.getResponseHeaders().set("Content-Type", "text/plain");
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server running on port 3000");
+    }
+}
